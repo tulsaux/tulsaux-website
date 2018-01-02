3ba01c70756186dc65a1cd39d732b2637ab2222e
Maxx Crawford <maxx.crawford@gmail.com>
2018-01-01 21:05:41 -0600

    Resolved iOS mobile padding issue

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+(function () {
+  // Really basic check for the ios platform
+  // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+  // Get the device pixel ratio
+  var ratio = window.devicePixelRatio || 1;
+
+  // Define the users device screen dimensions
+  var screen = {
+    width: window.screen.width * ratio,
+    height: window.screen.height * ratio
+  };
+
+  // iPhone X Detection
+  if (iOS && screen.width == 1125 && screen.height === 2436) {
+    document.body.className += ' ' + 'ios-safearea-exists';
+  } else {
+    console.log('no-notch');
+  }
+})();
