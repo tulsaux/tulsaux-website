30e4239eb9d8e0aa7b11944d21b060a3d51516db
Maxx Crawford <maxx.crawford@gmail.com>
2020-08-03 15:18:16 -0500

    Added events through January, added remote-only banner alert, revised wording to point users to 36N

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,7 +50,12 @@ window.setTimeout( ()=> {
   const expiry = new Date(2020, 1, 13);
   const expiry2 = new Date(2020, 2, 10);
   const expiry3 = new Date(2020, 3, 14);
-
+  const expiry4 = new Date(2020, 7, 11);
+  const expiry5 = new Date(2020, 8, 8);
+  const expiry6 = new Date(2020, 9, 13);
+  const expiry7 = new Date(2020, 10, 10);
+  const expiry8 = new Date(2020, 11, 8);
+  const expiry9 = new Date(2021, 0, 12);
 
   const eventInfoPlaceholder = document.getElementById("eventInfoPlaceholder");
   const eventInfoFeb = document.getElementById("eventInfoFeb");
@@ -58,8 +63,26 @@ window.setTimeout( ()=> {
   const eventInfoApr = document.getElementById("eventInfoApr");
   const eventInfoMay = document.getElementById("eventInfoMay");
 
-  if (current.getTime() > expiry3.getTime()) {
-    eventInfoMay.style.display = "block";
+  const eventInfoAug = document.getElementById("eventInfoAug");
+  const eventInfoSept = document.getElementById("eventInfoSept");
+  const eventInfoOct = document.getElementById("eventInfoOct");
+  const eventInfoNov = document.getElementById("eventInfoNov");
+  const eventInfoDec = document.getElementById("eventInfoDec");
+  const eventInfoJan = document.getElementById("eventInfoJan");
+
+  if (current.getTime() > expiry9.getTime()) {
+  } else if (current.getTime() > expiry8.getTime()) {
+    eventInfoJan.style.display = "block";
+  } else if (current.getTime() > expiry7.getTime()) {
+    eventInfoDec.style.display = "block";
+  } else if (current.getTime() > expiry6.getTime()) {
+    eventInfoNov.style.display = "block";
+  } else if (current.getTime() > expiry5.getTime()) {
+    eventInfoOct.style.display = "block";
+  } else if (current.getTime() > expiry4.getTime()) {
+    eventInfoSept.style.display = "block";
+  } else if (current.getTime() > expiry3.getTime()) {
+    eventInfoAug.style.display = "block";
   } else if (current.getTime() > expiry2.getTime()) {
     eventInfoApr.style.display = "block";
   } else if (current.getTime() > expiry.getTime()) {
